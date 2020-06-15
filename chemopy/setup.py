10fc7f7b30f124a41cf8df6d48e4be1e9f02f963
CoMorvan <coren.morvan@gmail.com>
2020-06-15 10:13:41 +0200

    Add chemopy

---
+++ chemopy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='chempopy',
+      version='0.1',
+      description='Features extraction for C-SGEN model',
+      author='Corentin Morvan',
+      author_email='corentin.morvan@ens-lyon.fr',
+      license='MIT',
+      packages=['chemopy'],
+      zip_safe=False)
